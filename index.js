23960f65d7d8107e25bb5f404a327c80760e1870
chiabi <chiabi88@gmail.com>
2018-07-17 12:34:46 +0900

    Add definite schema and query types

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
+import { makeExecutableSchema } from "graphql-tools";
+
+// Dummy DATA 추후 DB 자료와 교체
+// 먼저 자료구조를 정의
+const books = [
+  { title: "하루3분 네트워크 교실", author: "아미노 에이지" },
+  { title: "You Don't know JS", author: "카일 심슨" },
+  { title: "함수형 자바스크립트", author: "루이스 아텐시오" }
+];
+
+// 쿼리 정리, Book 배열 정의
+const typeDefs = `
+  type Query {
+    books : [Book]
+  }
+  type Book {
+    title: String
+    author: String
+  }
+`;
+const schema = makeExecutableSchema({
+  typeDefs
+});
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("하하하호호호");
-});
+app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
+
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+
 app.listen(3000, () => {
   console.log("서버가 시작되었습니다!");
 });
